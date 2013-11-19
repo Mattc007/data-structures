6133c19854b6386c2553befded242d199cf371d1
Matt Catrett <mcatrett@turbosquid.com>
2013-11-18 20:51:02 -0600

    full stack

--- lib/data_structures/stack.rb
+++ lib/data_structures/stack.rb
@@ -1,6 +1,7 @@
 module DataStructures
   class Stack
-    attr_reader :length, :top
+
+    attr_reader :top
 
     def initialize(size = 1)
       @length = size
@@ -8,18 +9,31 @@ module DataStructures
     end
 
     def push(element)
+      raise 'stack overflow' if filled?
       @array.push element
+      @top = @top ? @top + 1 : 1
+      self
     end
 
     def pop
+      raise 'stack underflow' if empty? || @top.nil?
+      @top = @top == 1 ? @top = nil : @top - 1
       @array.pop
     end
+
+    def empty?
+      @top == 0
+    end    
 
     private
     
     def reset
       @array = []*@length
       @top = nil
+    end
+
+    def filled?
+      @top == @length
     end
   end
 end
--- test/stack_test.rb
+++ test/stack_test.rb
@@ -2,9 +2,33 @@ require 'test/unit'
 require_relative '../lib/data_structures'
 
 class TestStack < Test::Unit::TestCase
+
+  def setup
+    @stack = DataStructures::Stack.new(3)
+  end
+
   def test_stack
-    assert_equal(nil, DataStructures::Stack.new(10).top)
-    assert_equal('a', DataStructures::Stack.new(10).push('a').pop)
+    assert !@stack.top
+
+    @stack.push 1
+    assert_equal 1, @stack.pop
+
+    @stack.push 1
+    @stack.push 2
+    assert_equal 2, @stack.pop
+    assert_equal 1, @stack.pop
+
+    assert_raise RuntimeError, 'stack underflow' do
+      @stack.pop
+    end
+
+    @stack.push 1
+    @stack.push 2
+    @stack.push 3
+    assert_raise RuntimeError, 'stack overflow' do
+      @stack.push 4
+    end
+
   end
 end
 
